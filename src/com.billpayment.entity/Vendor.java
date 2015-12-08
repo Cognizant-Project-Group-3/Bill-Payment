952bf36b5f4e42c1a7c839c32226c8ac8aa53137
mshekdar <shekdarm@gmail.com>
2015-12-08 11:33:07 +0530

    Create Vendor.java

---
+++ src/com.billpayment.entity/Vendor.java
@@ -0,0 +1,17 @@
+package com.billpayment.entity;
+
+
+public class Vendor {
+      String name;
+      
+      String type;
+      String company_reg_no;
+      String address;
+      String country;
+      String emailid;
+      String contactno;
+      String website;
+      String issue_date;
+      String validity_date;
+      int yoe;
+}
