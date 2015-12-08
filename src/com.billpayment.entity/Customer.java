5cc9774784b8e8ae7cd931dc73c20157b08f01ca
neha-deshmukh <neha.deshmukh0611@gmail.com>
2015-12-08 16:43:58 +0530

    Update Customer.java

--- src/com.billpayment.entity/Customer.java
+++ src/com.billpayment.entity/Customer.java
@@ -1,5 +1,72 @@
 package com.billpayment.entity;
 
 public class Customer {
+	private int id;
+	private String name;
+	private String contact_no;
+	private String address;
+	private String reg_date;
+	private String Document_detail_no;
+	private String email;
+	private String country_id;
+	private String card_no;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContact_no() {
+		return contact_no;
+	}
+	public void setContact_no(String contact_no) {
+		this.contact_no = contact_no;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getDocument_detail_no() {
+		return Document_detail_no;
+	}
+	public void setDocument_detail_no(String document_detail_no) {
+		Document_detail_no = document_detail_no;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCountry_id() {
+		return country_id;
+	}
+	public void setCountry_id(String country_id) {
+		this.country_id = country_id;
+	}
+	public String getCard_no() {
+		return card_no;
+	}
+	public void setCard_no(String card_no) {
+		this.card_no = card_no;
+	}
+	
+	
+	
+
 
 }
