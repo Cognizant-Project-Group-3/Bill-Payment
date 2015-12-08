e9065bd0349e228ac71a42053827c402cf18b031
mshekdar <shekdarm@gmail.com>
2015-12-08 11:34:22 +0530

    Create CustomerException.java

---
+++ src/com.billpayment.exception/CustomerException.java
@@ -0,0 +1,13 @@
+package com.billpayment.exception;
+
+public class CustomerException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4873882837220842584L;
+
+	public CustomerException(String message){
+		super(message);
+	}
+}
