13463bafb1187953d8d079c22fc07cd60115c111
Shefali Singhal <shefalisinghal659@gmail.com>
2015-12-10 14:18:01 +0530

    Update AdminBO.java

--- src/com.billpayment.BO/AdminBO.java
+++ src/com.billpayment.BO/AdminBO.java
@@ -1,13 +1,32 @@
 package com.billpayment.BO;
 
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
+
 import com.billpayment.dao.AdminDAO;
+import com.billpayment.exception.DatabaseException;
 
 public class AdminBO {
 
-	public boolean verify(String username,String password){
-		AdminDAO isadmin=new AdminDAO();
+	private Logger LOGGER = Logger.getLogger(com.billpayment.BO.AdminBO.class);
+
+	public boolean verify(String username, String password)
+			throws DatabaseException {
+		AdminDAO isadmin = new AdminDAO();
 		boolean status;
-		status=isadmin.verify(username,password);
+
+		try {
+			status = isadmin.verify(username, password);
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			LOGGER.error("Jar for Oracle 10g not found.");
+			throw new DatabaseException(e);
+		} catch (SQLException e) {
+			// TODO: handle exception
+			LOGGER.error("Exception related to database");
+			throw new DatabaseException(e);
+		}
 		return status;
 	}
 }
