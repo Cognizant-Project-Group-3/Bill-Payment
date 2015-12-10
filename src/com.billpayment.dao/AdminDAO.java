73ceb4fddfa75ec7eb9f074b75087914d3c36b63
Shefali Singhal <shefalisinghal659@gmail.com>
2015-12-10 14:17:24 +0530

    Update AdminDAO.java

--- src/com.billpayment.dao/AdminDAO.java
+++ src/com.billpayment.dao/AdminDAO.java
@@ -1,51 +1,44 @@
 package com.billpayment.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import com.billpayment.helper.DBUtil;
 
 public class AdminDAO {
-	private Connection con;
-	public boolean verify(String username,String password){//String username,String password
-		boolean status=false;
-		Statement stmt = null;
-		String sql="SELECT USERNAME,PASSWORD FROM ADMIN";
-		System.out.println("IN function");
-		try{
-			con=DBUtil.createConnection();
-			stmt = con.createStatement();
-			System.out.println("Inside1");
-			try {
-				ResultSet rs = stmt.executeQuery(sql);
-				System.out.println("Inside2");
-				System.out.println(rs);
-		        while (rs.next()) {
-		        	System.out.println(rs.getString("USERNAME")+" "+rs.getString("PASSWORD"));
-		        	if(rs.getString("USERNAME")==username && rs.getString("PASSWORD")==password)
-		        		System.out.println("Inside3");
-		        		status=true;
-			} }catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-			
-			
-	        
-		}
-		catch(ClassNotFoundException cnfe){
-			System.out.println("JDBC driver for Oracle 10g not present");
-		}
-		catch(SQLException se){
-			System.out.println(se.getMessage());
-			}
-		finally{
-			return status;
-			//DBUtil.closeConnection();
-			
-		}
 
-}
+	public boolean verify(final String username, final String password)
+			throws ClassNotFoundException, SQLException {// String
+		// username,String
+		// password
+		Connection con = null;
+		boolean status = false;
+
+		try {
+			con = DBUtil.createConnection();
+			final PreparedStatement stmt = con
+					.prepareStatement("SELECT USERNAME,PASSWORD FROM ADMIN");
+
+			final ResultSet rsAdmin = stmt.executeQuery();
+
+			while (rsAdmin.next()) {
+				if (rsAdmin.getString("USERNAME").equalsIgnoreCase(username)
+						&& rsAdmin.getString("PASSWORD").equals(password)) {
+					status = true;
+
+					break;
+				}
+			}
+			rsAdmin.close();
+		} finally {
+			DBUtil.closeConnection();
+			if (con != null) {
+				con.close();
+
+			}
+		}
+		return status;
+	}
 }
