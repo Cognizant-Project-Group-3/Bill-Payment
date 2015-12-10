63542f7998d62f48da2e7a4497d7ab47f9868d9e
Shefali Singhal <shefalisinghal659@gmail.com>
2015-12-10 14:23:39 +0530

    Update Customer.java

--- src/com.billpayment.entity/Customer.java
+++ src/com.billpayment.entity/Customer.java
@@ -10,63 +10,97 @@ public class Customer {
 	private String email;
 	private String country_id;
 	private String card_no;
+
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(int id, String name, String contact_no, String address,
+			String reg_date, String document_detail_no, String email,
+			String country_id, String card_no) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.contact_no = contact_no;
+		this.address = address;
+		this.reg_date = reg_date;
+		Document_detail_no = document_detail_no;
+		this.email = email;
+		this.country_id = country_id;
+		this.card_no = card_no;
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getContact_no() {
 		return contact_no;
 	}
+
 	public void setContact_no(String contact_no) {
 		this.contact_no = contact_no;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getReg_date() {
 		return reg_date;
 	}
+
 	public void setReg_date(String reg_date) {
 		this.reg_date = reg_date;
 	}
+
 	public String getDocument_detail_no() {
 		return Document_detail_no;
 	}
+
 	public void setDocument_detail_no(String document_detail_no) {
 		Document_detail_no = document_detail_no;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getCountry_id() {
 		return country_id;
 	}
+
 	public void setCountry_id(String country_id) {
 		this.country_id = country_id;
 	}
+
 	public String getCard_no() {
 		return card_no;
 	}
+
 	public void setCard_no(String card_no) {
 		this.card_no = card_no;
 	}
-	
-	
-	
-
 
 }
